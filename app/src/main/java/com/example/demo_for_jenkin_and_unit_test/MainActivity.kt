95462bdbee48170b8a2ca3b9af36a3c7dcc47df0
Mustofa Mahmud <mustofa.mahmud@bjitgroup.com>
2024-08-29 14:45:50 +0600

    initial commit

---
+++ app/src/main/java/com/example/demo_for_jenkin_and_unit_test/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.demo_for_jenkin_and_unit_test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
