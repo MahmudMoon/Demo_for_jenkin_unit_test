1fc19e809ee003f34e748118683def8aab4cc2b9
Mustofa Mahmud <mustofa.mahmud@bjitgroup.com>
2024-08-29 14:59:50 +0600

    log added

--- app/src/main/java/com/example/demo_for_jenkin_and_unit_test/MainActivity.kt
+++ app/src/main/java/com/example/demo_for_jenkin_and_unit_test/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.demo_for_jenkin_and_unit_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate: TEST log added")
     }
 }
